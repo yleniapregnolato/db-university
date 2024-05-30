3bf49065e6b3473c579685a8cc634eee329c59fd
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-30 17:26:05 +0200

    feat: query 3 group by

--- query_GROUP_BY.sql
+++ query_GROUP_BY.sql
@@ -8,3 +8,10 @@ ORDER BY `year_of_enrolment`;
 SELECT `teachers`.`office_address`, COUNT(*) AS `teachers_number`
 FROM `teachers`
 GROUP BY `office_address`
+
+-- Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_student`.`exam_id` AS `id`, AVG(`exam_student`.`vote`) AS `avarage_grades`
+FROM `exam_student`
+GROUP BY `id`
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento
