5db45320d9311bd041b99295c36b34caa1d5853f
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-30 17:46:57 +0200

    fix: query 1 join

--- query_JOIN.sql
+++ query_JOIN.sql
@@ -1,6 +1,8 @@
 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `students`.`degree_id`
 FROM `students`
-JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `degrees` 
+ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
+-- Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
